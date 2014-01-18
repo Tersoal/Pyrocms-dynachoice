7532522532d29ce1aa278805c0fc82c6ef52f7dd
Tersoal <tersoald20@gmail.com>
2014-01-18 15:55:44 +0100

    Initial project

---
+++ language/spanish/dynachoice_lang.php
@@ -0,0 +1,17 @@
+<?php defined('BASEPATH') or exit('No direct script access allowed');
+
+$lang['streams:dynachoice.name'] 				= 'Selección dinámica';
+$lang['streams:dynachoice.instructions'] 		= 'Nothing';
+$lang['streams:dynachoice.choice_data'] 		= 'Datos de Selección';
+$lang['streams:dynachoice.stream_slug'] 		= 'Stream asociado';
+$lang['streams:dynachoice.choice_type'] 		= 'Tipo de Selección';
+$lang['streams:dynachoice.dropdown'] 			= 'Desplegable';
+$lang['streams:dynachoice.radio_buttons'] 		= 'Botones Radio';
+$lang['streams:dynachoice.checkboxes'] 			= 'Casilla de Verificación';
+$lang['streams:dynachoice.min_choices'] 		= 'Nº mínimo de selecciones'; 
+$lang['streams:dynachoice.max_choices'] 		= 'Nº máximo de selecciones'; 
+$lang['streams:dynachoice.checkboxes_only'] 	= 'Solo disponible para casillas de verificación.'; 
+$lang['streams:dynachoice.must_select_num'] 	= 'Debes seleccionar {val} datos de la lista de %s.';				
+$lang['streams:dynachoice.must_at_least'] 		= 'Debes seleccionar al menos {val} datos de la lista de %s.';		
+$lang['streams:dynachoice.must_max_num'] 		= 'Solo puedes seleccionar {val} datos de la lista de %s.';			
+$lang['streams:dynachoice.multiselect'] 		= 'Tipo multiselección.'; 											
